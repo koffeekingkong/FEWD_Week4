cd0d4764316265336ee5e5a22f100726d56a4600
koffeekingkong <koffeekingkong@gmail.com>
2016-01-16 15:57:21 +0800

    Traffic light exercise 1
    Red traffic light to illuminate now.

--- week4_working_files/jquery_traffic_light_exercise/js/index.js
+++ week4_working_files/jquery_traffic_light_exercise/js/index.js
@@ -1,1 +1,34 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+
+// vanilla js
+// document.getElementById('stopButton').onclick = illuminateRed;
+
+// jQuery
+$("#stopButton").click(illuminateRed);
+// $("#slowButton").click(illuminateYellow);
+// $("#goButton").click(illuminateGreen);
+
+
+function illuminateRed() {
+  clearLights();
+  // document.getElementById('stopLight').style.backgroundColor = "red";
+  $("#stopLight").css("backgroundColor","red");
+}
+// function illuminateYellow() {
+//   clearLights();
+//   $("#slowLight").css("backgroundColor","yellow");
+// }
+// function illuminateGreen() {
+//   clearLights();
+//   $("#goLight").css("backgroundColor","green");
+// }
+
+function clearLights() {
+  // document.getElementById('stopLight').style.backgroundColor = "black";
+  // document.getElementById('slowLight').style.backgroundColor = "black";
+  // document.getElementById('goLight').style.backgroundColor = "black";
+
+$("#stopLight").css("backgroundColor","black");
+$("#slowLight").css("backgroundColor","black");
+$("#goLight").css("backgroundColor","black");
+}
